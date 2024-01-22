09625c7745885d6544baece64ae6ec1388dbd741
Keshav Dayal <115068840+keshxvdayal@users.noreply.github.com>
2024-01-22 16:30:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Service from './components/Service/service.jsx';
 import Project from './components/Projects/project.jsx';
 import Testimonial from './components/Testimonials/testimonial.jsx';
 import Contact from './components/ContactForm/contact.jsx';
-import Navbar from './components/Navbar/navbar.jsx'
 
 function App() {
 
 
   return (
       <main className='main' >
-        <Navbar/>
         <Home />
         <About />
         <Service />
